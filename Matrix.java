253ad7a5c0a0e3a66054499d6fff02255597733d
asaadco <31670621+asaadco@users.noreply.github.com>
2019-03-22 00:50:07 +0300

    Added longArray

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,4 @@
-
+import sun.misc.Unsafe;
 public class Matrix{
     public static int[][] iterative(int[][] a, int[][] b, int n){
         int[][] c = new int[n][n];
@@ -35,4 +35,24 @@ public class Matrix{
         print(c);
 
     }
+    class largeArray{
+        private static final int BYTE = 4;
+        private long size;
+        private long address;
+        public largeArray(long size){
+            this.size = size;
+            address = getUnsafe().allocateMemory(size*size*BYTE);
+        }
+        public void set(long i, long j, int value){
+            getUnSafe().putByte(address + (i*size+j)*BYTE, value);
+        }
+        public int get(long i, long j){
+            return getUnSafe().getByte(address + (i*size+j)*BYTE);
+    
+        }
+        public long size() {
+            return size;
+        }
+            
+        }
 }

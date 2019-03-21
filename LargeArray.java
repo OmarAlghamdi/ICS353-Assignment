306213f7d3370c3db44ea3a8e6b60a99bdc37abc
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-03-22 01:22:04 +0300

    compilable largeArray

--- LargeArray.java
+++ LargeArray.java
@@ -1,21 +1,19 @@
-import sun.misc.Unsafe;
-class largeArray{
+import  sun.misc.Unsafe;
+class LargeArray{
     private static final int BYTE = 4;
     private long size;
     private long address;
-    public largeArray(long size){
+    public LargeArray(long size){
         this.size = size;
-        address = getUnsafe().allocateMemory(size*size*BYTE);
+        address = Unsafe.getUnsafe().allocateMemory(size*size*BYTE);
     }
     public void set(long i, long j, int value){
-        getUnSafe().putByte(address + (i*size+j)*BYTE, value);
+        Unsafe.getUnsafe().putInt(address + (i*size+j)*BYTE, value);
     }
     public int get(long i, long j){
-        return getUnSafe().getByte(address + (i*size+j)*BYTE);
-
+        return Unsafe.getUnsafe().getInt(address + (i*size+j)*BYTE);
     }
     public long size() {
         return size;
     }
-        
 }

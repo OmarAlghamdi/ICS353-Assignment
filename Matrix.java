f8ffafa9fd3cc08f7fba683a528b4f3df09f8de0
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-03-25 09:52:39 +0300

    Incomplete Strassen incomplete implementation of Strassen Algorithm. it still needs: . define the base case flow . implement a method that partition a matrix to 4 regions . implement a method that combimne 4 matrices into one . do the recursive step
    NOTE: additions and subtractions are better done before the recursive
        step to avoid inefficint repetetions

--- Matrix.java
+++ Matrix.java
@@ -52,4 +52,51 @@ public class Matrix{
             return b;
         }
     }
+    // NOTE: it is not efficint to add & subtract m1..m7 during multiplcation step
+    public static long[][] strassen(long[][] a, long[][] b, int n, int base){
+        // TODO define base case flow
+        a = matrixPadding(a, n);
+        b = matrixPadding(b, n);
+        int m = n/2;
+        long[][] a11, a12, a21, a22;
+        long[][] b11, b12, b21, b22;
+        long[][] m1, m2, m3, m4, m5, m6, m7;
+        long[][] c11, c12, c21, c22;
+        // initalizations
+        m1 = new long[m][m]; m2 = new long[m][m]; m3 = new long[m][m];
+        m4 = new long[m][m]; m5 = new long[m][m]; m6 = new long[m][m];
+        m7 = new long[m][m];
+        a11 = new long[m][m]; a12 = new long[m][m];
+        a21 = new long[m][m]; a22 = new long[m][m];
+        b11 = new long[m][m]; b12 = new long[m][m];
+        b21 = new long[m][m]; b22 = new long[m][m];
+        c11 = new long[m][m]; c12 = new long[m][m];
+        c21 = new long[m][m]; c22 = new long[m][m];
+        
+        // TODO partition
+        // TODO multiply
+        // TODO combine
+        
+        return null;
+    }
+    // TODO partition method
+    // TODO combine method
+    public static long[][] matrixAddition(long[][] a, long[][] b, int n){
+        long[][] c = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return c;
+    }
+    public static long[][] matrixSubtraction(long[][] a, long[][] b, int n){
+        long[][] c = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                c[i][j] = a[i][j] - b[i][j];
+            }
+        }
+        return c;
+    }
 }

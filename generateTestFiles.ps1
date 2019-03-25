c3c3e95a98051c2973ef4eb9258f633da48042c8
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-03-26 01:24:17 +0300

    generates multiple test matrices it creates a new dir with multiple test matrics named according to thier sizes -minSize is sthe smallest matrix size -maxSize is the largest matrix size -step is the increment of matrix size -dir is the name of result directory

--- generateTestFiles.ps1
+++ generateTestFiles.ps1
@@ -1,4 +1,4 @@
-param([int32]$size, [string]$name)
+param([int32]$minSize, [int32]$maxSize, [int32]$step, [string]$dir)
 function rand
 {
     param([int32]$n)
@@ -9,10 +9,18 @@ function rand
     return $row
 }
 
+function generate
+{
+    param([int32]$size, [string]$name)
+    New-Item -Path $PSScriptRoot/$dir -Name "$name.txt" -ItemType "file" 
+    for ($i = 0; $i -lt $size*2; $i++) {
+        $r = rand $size
+        Add-Content -Path $PSScriptRoot/$dir/"$name.txt" -Value $r
+    }
+}
 
-New-Item -Path . -Name "$name.txt" -ItemType "file" 
-for ($i = 0; $i -lt $size*2; $i++) {
-    $r = rand $size
-    Add-Content -Path $PSScriptRoot/"$name.txt" -Value $r
+New-Item -Name $dir -ItemType "directory"
+for ($i = $minSize; $i -le $maxSize; $i += $step) {
+    generate $i "$i-$i-matrices"
 }
 

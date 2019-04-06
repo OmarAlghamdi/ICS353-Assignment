0ecc047a3c466669aa1eedb0ddc289708decae59
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-04-06 13:59:09 +0300

    extracting test results into CSV

--- toCSV.ps1
+++ toCSV.ps1
@@ -10,8 +10,17 @@ foreach ($file in $files)
     $ind = $str.IndexOf("-")
     $val = [string]$str.Substring($start, $ind) + "," 
     # base
+    $start = $ind + 1
+    $ind = $str.IndexOf([char]"-", [int32]$start)
+    $val = $val + [string]$str.Substring($start, $ind-$start) + ","
     # size
+    $start = $ind + 1
+    $ind = $str.IndexOf([char]"-", [int32]$start)
+    $val = $val + [string]$str.Substring($start, $ind-$start) + ","
     # time
+    $path = $resultsDir + "/" + $str
+    $time = Get-Content -Path $PSScriptRoot/$path -TotalCount 1
+    $val = $val + [string]$time
     # $time = Get-Content $file -ReadCount 1
 
     # write

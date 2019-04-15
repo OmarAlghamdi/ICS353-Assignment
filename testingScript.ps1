e2c8264a8ccf73e5d94b2db8c80ca6ae6a9281a4
asaadco <31670621+asaadco@users.noreply.github.com>
2019-04-15 23:42:43 +0300

    Changed driver to matrixMultiply. Report added.

--- testingScript.ps1
+++ testingScript.ps1
@@ -3,12 +3,12 @@ param([string]$inputDir, [string]$resultDir)
 function iterative
 {
     param([string]$fileName, [int32]$n)
-    & java.exe Driver 0 $n 0 $inputDir/$fileName $resultDir/"iterative-1-$fileName"
+    & java.exe matrixMultiply 0 $n 0 $inputDir/$fileName $resultDir/"iterative-1-$fileName"
 }
 function Strassen
 {
     param([string]$fileName, [int32]$base, [int32]$n)
-    & java.exe Driver 1 $n $base $inputDir/$fileName $resultDir/"Strassen-$base-$fileName"
+    & java.exe matrixMultiply 1 $n $base $inputDir/$fileName $resultDir/"Strassen-$base-$fileName"
 }
 
 New-Item -Path . -Name $resultDir -ItemType "directory" 
